48defc31d4510fd77017f9df9dac666f1b4cf464
Luis <luisflorez20@yahoo.com>
2025-07-28 15:01:41 -0500

    Initial commit - Request Header Parser Microservice

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// new API endpoint: /api/whoami
+app.get("/api/whoami", function (req, res) {
+  const ip = req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({
+    ipaddress: ip,
+    language: language,
+    software: software
+  });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
